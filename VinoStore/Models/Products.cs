cd5e73371330f3445cb476d165b671dcd3b00e87
Dal4n <21000028@alumnos.utleon.edu.mx>
2024-07-06 19:03:51 -0600

    Nuevo

---
+++ VinoStore/Models/Products.cs
@@ -0,0 +1,13 @@
+﻿namespace VinoStore.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+        public string ImagenURL { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+}
---
+++ VinoStore/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using VinoStore.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+/* SE OBTIENE LA CADENA DE CONEXCION */
+var cadenaConexion = builder.Configuration.GetConnectionString("bd_tareas");
+
+/* CONFIGURAMOS SERVICIO PARA USAR SQL SERVER */
+builder.Services.AddDbContext<BdTareas902Context>(options =>
+options.UseSqlServer(cadenaConexion));
+
+/* CONFIGURACION DEL CORS */
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("NuevaPolitica", app =>
+    {
+        app.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
